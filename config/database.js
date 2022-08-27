60b0d5de6c597ece03007cc3aa7a5e1e8c1a538c
sayeem <sayeems@gmail.com>
2022-08-27 21:37:50 +0600

    mysql added

--- config/database.js
+++ config/database.js
@@ -1,11 +1,33 @@
-const path = require('path');
+// ## sqlite
+// const path = require('path');
+
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: 'sqlite',
+//     connection: {
+//       filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+//     },
+//     useNullAsDefault: true,
+//   },
+// });
+
+// mysql
+
+// path: ./config/database.js
 
 module.exports = ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: "mysql",
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      host: env("DATABASE_HOST", "127.0.0.1"),
+      port: env.int("DATABASE_PORT", 8889),
+      database: env("DATABASE_NAME", "strapi4"),
+      user: env("DATABASE_USERNAME", "root"),
+      password: env("DATABASE_PASSWORD", "root"),
+      ssl: {
+        rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false), // For self-signed certificates
+      },
     },
-    useNullAsDefault: true,
+    debug: true,
   },
 });
